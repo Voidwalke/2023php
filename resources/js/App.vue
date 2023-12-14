6d95783f121025f712dcfbf2040ad4b623f70bd7
Voidwalke <80790906+Voidwalke@users.noreply.github.com>
2023-12-14 14:10:34 +0300

    Add files via upload

---
+++ resources/js/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div v-show="this.msg != null " >
+        Добавлена новая статья 
+        <strong>
+            <a href="article/4">
+
+            </a>
+            {{ this.msg }}
+        </strong>
+    </div>
+</template>
+<script>
+    export default{
+        data() {
+            return{msg:null}
+        },
+        created(){
+            window.Echo.channel('test').listen('EventNewArticle', (name)=>{
+                console.log(name);
+                this.msg=name.article.title;
+                
+            })
+        }
+    }
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+require('./bootstrap');
+
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp({})
+
+app.component('App', App)
+
+app.mount('#app')
